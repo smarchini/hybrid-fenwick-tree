c74d11ce99b6d8a4c25feb25b5831d7768981af2
Stefano Marchini <stefano.marchini@studenti.unimi.it>
2017-04-17 04:16:13 +0200

    Linear or semilogx with command line arguments

--- utils/plot.py
+++ utils/plot.py
@@ -6,23 +6,17 @@ def plot(infile, outfile):
         reader = csv.reader(csvfile, delimiter=',')
         elements = [ r for r in reader ]
         xdata = [ int(elements[i][0]) for i in range(1, len(elements)) ]
-        ydata1 = [ float(elements[i][1]) for i in range(1, len(elements)) ]
-        ydata2 = [ float(elements[i][2]) for i in range(1, len(elements)) ]
-        ydata3 = [ float(elements[i][3]) for i in range(1, len(elements)) ]
-        ydata4 = [ float(elements[i][4]) for i in range(1, len(elements)) ]
-        ydata5 = [ float(elements[i][5]) for i in range(1, len(elements)) ]
-        ydata6 = [ float(elements[i][5]) for i in range(1, len(elements)) ]
+
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
-        ax.semilogx(xdata, ydata1, label='simple')
-        ax.semilogx(xdata, ydata2, label='typed')
-        ax.semilogx(xdata, ydata3, label='ityped')
-        ax.semilogx(xdata, ydata5, label='byte')
-        ax.semilogx(xdata, ydata6, label='compact')
-        ax.semilogx(xdata, ydata6, label='shrank')
+
+        for j in range(1, len(elements[0])):
+            ydata = [ float(elements[i][j]) for i in range(1, len(elements)) ]
+            ax.plot(xdata, ydata, label=elements[0][j])
+
         plt.legend(loc="upper left", bbox_to_anchor=[0, 1], ncol=2, shadow=True, fancybox=True)
         plt.savefig(outfile)
         plt.show()
 
 if __name__ == '__main__':
-    plot(sys.argv[1], 'prova.png')
+    plot(sys.argv[1], 'linear.png')
